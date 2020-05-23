ab5d6cb389e89ac3e22c3f43383bc6d786c23e38
6byte <444891953@qq.com>
2020-05-23 14:48:12 +0800

    05-23-06

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	// 再次访问不弹出提示框
-	localStorage.setItem("review","true")
+	
 	let review = localStorage.getItem("review")
 	if(func.isEmpty(review)){
+		console.log(review);
 		openModal(messageIndex.INDEX)
+		localStorage.setItem("review","true")
 	}
 	
 	
